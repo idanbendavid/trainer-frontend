972e421e65b4d30d6f144661cf27f8fcdc9b7db8
idanbendavid <idanbendavid10@hotmail.com>
2022-08-13 13:32:24 +0300

    convert dates to strings function

---
+++ src/helpers/dateHelper.ts
@@ -0,0 +1,19 @@
+export function dateHepler(mutableDate: Date) {
+
+    let date = mutableDate.getDate();
+    let month = mutableDate.getMonth() + 1;
+    let year = mutableDate.getFullYear();
+
+    let fixedDate: string;
+
+    if (month < 10) {
+        let fixedMonth = `0${month}`
+        fixedDate = year + "-" + fixedMonth + "-" + date;
+
+        return fixedDate
+    }
+    else {
+        fixedDate = year + "-" + month + "-" + date;
+        return fixedDate
+    }
+}
