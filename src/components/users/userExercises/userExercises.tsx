848cc3a1c6b63ea3cf9f0c83fe8f90d63759b0fe
idanbendavid <idanbendavid10@hotmail.com>
2022-08-20 16:43:03 +0300

    changed from dataGrid to cards with compatible css

--- src/components/users/userExercises/userExercises.tsx
+++ src/components/users/userExercises/userExercises.tsx
@@ -1,33 +1,33 @@
-import { DataGrid } from '@mui/x-data-grid';
+import { Button, Card, Dialog, DialogContent, DialogTitle } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getExercisesOfUser } from '../../../features/exercises/exerciseSlice';
-import { useAppSelector } from '../../../store'
+import { getExercisesOfUser, deleteExerciseOfUser } from '../../../features/exercises/exerciseSlice';
+import { AppDispatch, useAppSelector } from '../../../store'
 import "./userExercises.css"
 
 function UserExercise() {
 
-  const dispatch = useDispatch<any>();
+  const dispatch = useDispatch<AppDispatch>();
+  const [checkStatusModal, setCheckStatusModal] = useState(Boolean);
+  const [exerciseToDelete, setExerciseToDelete] = useState(null);
 
-  // let user = useAppSelector((state) => state.auth.connectedUser);
   let userExercises = useAppSelector((state) => state.exercises.userExercises)
-  const [tableData, setTableData] = useState([])
-
-  const columns = [
-    { field: 'exercise_id', headerName: 'Exercise Id', width: 150 },
-    { field: 'bodyPart', headerName: 'Body Part', width: 150 },
-    { field: 'equipment', headerName: 'Equipment', width: 150 },
-    { field: 'name', headerName: 'Name', width: 350 },
-    { field: 'target', headerName: 'Target', width: 200 },
-    { field: 'exerciseDate', headerName: 'Exercise Date', width: 200 },
-  ]
-
 
   useEffect(() => {
 
     dispatch(getExercisesOfUser())
-    setTableData(userExercises)
-  }, [dispatch, userExercises])
+  }, [dispatch])
+
+  function checkExerciseStatus(exerciseOfUser) {
+    setCheckStatusModal(true);
+    setExerciseToDelete(exerciseOfUser)
+  }
+
+  function deleteExerciseFromUserSchedule() {
+    let exerciseId = exerciseToDelete.exercise_id
+
+    dispatch(deleteExerciseOfUser(exerciseId))
+  }
 
 
   return (
@@ -36,28 +36,42 @@ function UserExercise() {
         <h1>hard work dedication never quiting and you're looking at a champion</h1>
       </div>
       <div className='user-exercise-grid-split'>
-        <div className='user-exercises-list'>
-          <DataGrid
-            autoHeight
-            autoPageSize={true}
-            rows={tableData}
-            columns={columns}
-            getRowId={(row) => row.exercise_id}
-            pageSize={3}
-            rowsPerPageOptions={[3]}
-            checkboxSelection
-            onSelectionModelChange={(ids) => {
-              const selectedIDs = new Set(ids);
-              const selectedRowData = tableData.filter((tableData) =>
-                selectedIDs.has(tableData.exercise_id)
-              );
-              console.log(selectedRowData)
-            }} />
-        </div>
         <div className='single-exercise-detailed'>
+          {userExercises.map((exerciseOfUser: any, index: number) => {
+            return <Card className="user-exercise-display" key={index}>
+              <img src={exerciseOfUser.gifUrl} alt="exercise" />
+              <div>
+                <p className='user-exercises-exercise-name'>{exerciseOfUser.name}</p>
+                <p>{exerciseOfUser.target}</p>
+              </div>
+              <div>
+                <p>{exerciseOfUser.bodyPart}</p>
+                <p>{exerciseOfUser.equipment}</p>
+                <p>{exerciseOfUser.exerciseDate}</p>
+              </div>
+              <div className='actions-new-button'>
+                <Button variant='contained' onClick={() => checkExerciseStatus(exerciseOfUser)}>Was it done?</Button>
+              </div>
+            </Card>
+          })}
         </div>
-
       </div>
+      {checkStatusModal &&
+        <div >
+          <Dialog open={checkStatusModal} onClose={(reason: "backdropClick" | "escapeKeyDown") => setCheckStatusModal(false)} maxWidth='xs'>
+            <DialogTitle>
+              have you completed this exercise?
+            </DialogTitle>
+            <DialogContent>
+              <div className='check-exercise-status-dialog'>
+                <Button variant='contained' color='success' onClick={deleteExerciseFromUserSchedule}>Yes</Button>
+                <Button variant='contained' color='error' onClick={() => setCheckStatusModal(false)}>No</Button>
+              </div>
+              <h3>*By clicking Yes the exercise you selected will be deleted from your schdule</h3>
+            </DialogContent>
+          </Dialog>
+        </div>
+      }
     </div>
   )
 }
