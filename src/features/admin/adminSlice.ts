d908f548a9f3ef9c9229f06d0b7e9abe56a3474f
idanbendavid <idanbendavid10@hotmail.com>
2022-08-27 14:05:27 +0300

    new complaints function added

--- src/features/admin/adminSlice.ts
+++ src/features/admin/adminSlice.ts
@@ -1,11 +1,23 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import { toast } from 'react-toastify';
+import { IComplaint } from '../../models/IComplaint';
 import adminService from '../../services/adminService'
+import publicComplatinsService from '../../services/publicComplaints';
 
 const initialState = {
     message: "",
     isAdminLoggedIn: false,
     isError: false,
     isSuccess: false,
+    complaint: {
+        complaintId: 0,
+        first_name: "",
+        last_name: "",
+        email: "",
+        complaint_category: "",
+        description: ""
+    } as IComplaint,
+    publicComplaints: [] as IComplaint[]
 }
 
 
@@ -20,7 +32,28 @@ export const deleteUser = createAsyncThunk("users/delete", async (userToDelete: 
     }
 })
 
+export const getAllComplaints = createAsyncThunk("admin/getComplaints", async () => {
+    try {
+        const response = publicComplatinsService.getAllComplaints();
+        return response;
+    }
+    catch (error) {
+        const message: string = error.response.data.error;
+        return message
+    }
+})
 
+export const deleteUserComplaint = createAsyncThunk("admin/deleteComplaint", async (complaintId: number, thunkAPI) => {
+    try {
+        const response = publicComplatinsService.deleteComplaint(complaintId);
+        return response;
+    }
+    catch (error) {
+        const message: string = error.response.data.error;
+        toast.info(message)
+        return thunkAPI.rejectWithValue(message)
+    }
+})
 
 export const adminSlice = createSlice({
     name: "admin",
@@ -42,6 +75,19 @@ export const adminSlice = createSlice({
                 state.isSuccess = false
                 state.message = "can't delete user"
             })
+            // --------------------------------------------------------------
+            .addCase(getAllComplaints.fulfilled, (state, action: PayloadAction<IComplaint[]>) => {
+                state.publicComplaints = action.payload
+
+                state.publicComplaints.sort(function (a,b){
+                    return a.complaint_category.toLocaleLowerCase() > b.complaint_category.toLocaleLowerCase() 
+                    ? 1 : a.complaint_category.toLocaleLowerCase() < b.complaint_category.toLocaleLowerCase() ? -1 : 0
+                })
+            })
+            .addCase(getAllComplaints.rejected, (state, action: PayloadAction<{}>) => {
+                state.message = "could not get all user complaints please check passed data"
+            })
+
     }
 })
 
--- src/services/publicComplaints.ts
+++ src/services/publicComplaints.ts
@@ -3,24 +3,26 @@ import axios from "axios";
 async function getAllComplaints() {
     const response = await axios.get(`http://localhost:3001/complaint`)
 
-    console.log(response);
+    return response.data
+}
+
+async function newComplaint(userComplaint: object) {
+    const response = await axios.post(`http://localhost:3001/complaint`, userComplaint)
 
     return response.data
 }
 
 
-
-async function newComplaint(userComplaint: object) {
-    const response = await axios.post(`http://localhost:3001/complaint`, userComplaint)
-
-    console.log(response.data)
+async function deleteComplaint(complaintId: number) {
+    const response = await axios.delete(`http://localhost:3001/complaint/${complaintId}`)
 
     return response.data
 }
 
 const publicComplatinsService = {
-    getAllComplaints, 
-    newComplaint
+    getAllComplaints,
+    newComplaint,
+    deleteComplaint
 }
 
 
