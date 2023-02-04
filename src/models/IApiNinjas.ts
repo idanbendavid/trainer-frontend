5b86dfd9d4a1b3f23b4a763fc1026b1298debf9b
idanbendavid <idanbendavid10@hotmail.com>
2023-02-04 15:59:09 +0200

    interfaces changes
    change interface to match received api data

---
+++ src/models/IApiNinjas.ts
@@ -0,0 +1,8 @@
+export interface IApiNinjas {
+    name: string;
+    type: string;
+    muscle: string;
+    equipment: string;
+    difficulty: string;
+    instructions: string;
+}
--- src/models/IExercise.ts
+++
@@ -1,9 +0,0 @@
-export interface IExercise {
-    id: number
-    name: string
-    bodyPart: string
-    equipment: string
-    target: string
-    gifUrl: string
-
-}
