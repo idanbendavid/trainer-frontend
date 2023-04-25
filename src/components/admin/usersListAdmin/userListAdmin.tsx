f0516569cb569bbc1fbd97b0922db1afe0d03e1c
idanbendavid <idanbendavid10@hotmail.com>
2023-04-25 20:56:05 +0300

    remove delete user functions and unrelevant css

--- src/components/admin/usersListAdmin/userListAdmin.tsx
+++ src/components/admin/usersListAdmin/userListAdmin.tsx
@@ -1,32 +1,23 @@
-import { Button, Container, Dialog, DialogActions, DialogContent } from "@mui/material";
+import { Container } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { toast } from "react-toastify";
-import { deleteUser as deleteUserFromServer } from "../../../features/admin/adminSlice";
 import { UserRole } from "../../../models/role";
 import adminService from "../../../services/adminService";
-import { AppDispatch, useAppSelector } from "../../../store";
+import { useAppSelector } from "../../../store";
 import "./userListAdmin.css";
 
 function UsersListAdmin() {
 
-    const dispatch = useDispatch<AppDispatch>();
-
-    let token = useAppSelector((state) => state.auth.token);
+    let token = useAppSelector((state) => state.auth.connectedUser.token);
     let isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
     let userRole = useAppSelector((state) => state.auth.connectedUser.userRole);
 
     const [tableData, setTableData] = useState([])
-    const [deleteUserData, setDeleteUserData] = useState([]);
-    const [deleteConfarmation, setDeleteConfirmation] = useState(false);
-    const [showSelectionModel, setShowSelectionModel] = useState(false);
-
     const columns = [
-        { field: 'id', headerName: 'User Id', width: 100 },
-        { field: 'first_name', headerName: 'First Name', width: 150 },
-        { field: 'last_name', headerName: 'Last Name', width: 150 },
-        { field: 'birth_date', headerName: 'Birth Date', width: 150 },
+        { field: 'id', headerName: 'id', width: 100 },
+        { field: 'firstName', headerName: 'First Name', width: 150 },
+        { field: 'lastName', headerName: 'Last Name', width: 150 },
+        { field: 'birthDate', headerName: 'Birth Date', width: 150 },
         { field: 'email', headerName: 'Email', width: 250 },
     ]
 
@@ -41,39 +32,10 @@ function UsersListAdmin() {
         }
     }, [isLoggedIn, userRole, token])
 
-
-    function startOfDeleteUserProcess() {
-        setShowSelectionModel(true);
-        toast.info("please select the user you want to delete");
-    }
-
-    function completeUserDeletion() {
-        if (deleteUserData) {
-            dispatch(deleteUserFromServer(deleteUserData[0].id));
-            setDeleteConfirmation(false);
-            setShowSelectionModel(false);
-            setTableData(tableData.filter((user) => user.id !== deleteUserData[0].id));
-            deleteUserData.splice(0, deleteUserData.length)
-        }
-        deleteUserData.splice(0, deleteUserData.length)
-        setDeleteConfirmation(false);
-        setShowSelectionModel(false);
-    }
-
-    function cancelUserDeletion() {
-        deleteUserData.splice(0, deleteUserData.length)
-        setDeleteConfirmation(false);
-        toast.info("delete user was cancelled")
-    }
-
-
     return (
         <div className="user-list-admin-component">
             <h1 className="users-admin-h1">signed users</h1>
             <div className="users-list-admin">
-                <div className="delete-user-button">
-                    <Button variant="contained" color="error" onClick={startOfDeleteUserProcess}>Delete User</Button>
-                </div>
                 <Container component="main" maxWidth="md">
                     <DataGrid
                         autoHeight
@@ -81,29 +43,9 @@ function UsersListAdmin() {
                         columns={columns}
                         pageSize={10}
                         rowsPerPageOptions={[10]}
-                        checkboxSelection={showSelectionModel}
-                        onSelectionModelChange={(ids) => {
-                            const selectedIDs = new Set(ids);
-                            const selectedRowData = tableData.filter((tableData) =>
-                                selectedIDs.has(tableData.id)
-                            );
-                            setDeleteConfirmation(true);
-                            setDeleteUserData(selectedRowData);
-                        }}
                     />
                 </Container>
             </div>
-            {deleteUserData.length > 0 && deleteConfarmation &&
-                <Dialog open={deleteConfarmation} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
-                    <DialogContent>
-                        <h3>are you sure you want to delete the selected user?</h3>
-                    </DialogContent>
-                    <DialogActions className="user-deletion-selection">
-                        <Button onClick={completeUserDeletion}>Yes</Button>
-                        <Button onClick={cancelUserDeletion}>No</Button>
-                    </DialogActions>
-                </Dialog>
-            }
         </div>
     );
 }
