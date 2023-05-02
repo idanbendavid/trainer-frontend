be3444c0f214d9532e38cd1da43b59b8bc1ea4da
idanbendavid <idanbendavid10@hotmail.com>
2023-05-02 17:06:55 +0300

    users table for admin
    showing different count of number on table based on the screen size

--- src/components/admin/usersListAdmin/userListAdmin.tsx
+++ src/components/admin/usersListAdmin/userListAdmin.tsx
@@ -7,31 +7,46 @@ import { useAppSelector } from "../../../store";
 import "./userListAdmin.css";
 import React from "react";
 
+
 function UsersListAdmin() {
 
     let token = useAppSelector((state) => state.auth.connectedUser.token);
     let isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
     let userRole = useAppSelector((state) => state.auth.connectedUser.userRole);
 
-    const [tableData, setTableData] = useState([])
-    const columns = [
-        { field: 'id', headerName: 'id', width: 100 },
-        { field: 'firstName', headerName: 'First Name', width: 150 },
-        { field: 'lastName', headerName: 'Last Name', width: 150 },
-        { field: 'birthDate', headerName: 'Birth Date', width: 150 },
-        { field: 'email', headerName: 'Email', width: 250 },
-    ]
-
-    async function getAllUsers() {
-        const response = await adminService.getAllUsersForAdmin();
-        setTableData(response)
-    }
+    const [tableData, setTableData] = useState([]);
 
     useEffect(() => {
         if (isLoggedIn && userRole === UserRole.Admin) {
             getAllUsers();
         }
     }, [isLoggedIn, userRole, token])
+
+    async function getAllUsers() {
+        const response = await adminService.getAllUsersForAdmin();
+        setTableData(response)
+    }
+
+    function setTableColumns() {
+
+        let columns = [
+            { field: 'id', headerName: 'id', flex: 0.5 },
+            { field: 'firstName', headerName: 'First Name', flex: 1 },
+            { field: 'lastName', headerName: 'Last Name', flex: 1 },
+            { field: 'email', headerName: 'Email', flex: 1 },
+            { field: 'birthDate', headerName: 'Birth Date', flex: 1 },
+        ];
+
+        if (window.innerWidth > 600 && window.innerWidth < 750) {
+            let medCols = columns.slice(0, 4);
+            return medCols;
+        }
+        if (window.innerWidth < 600) {
+            let smCols = columns.slice(1, 4);
+            return smCols;
+        }
+        return columns;
+    }
 
     return (
         <div className="user-list-admin-component">
@@ -41,7 +56,7 @@ function UsersListAdmin() {
                     <DataGrid
                         autoHeight
                         rows={tableData}
-                        columns={columns}
+                        columns={setTableColumns()}
                         pageSize={10}
                         rowsPerPageOptions={[10]}
                     />
