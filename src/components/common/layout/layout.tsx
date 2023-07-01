2f83b8492bd8dd31747700396c0d47fa0adc54e9
idanbendavid <idanbendavid10@hotmail.com>
2023-07-01 16:17:06 +0300

    css changes

--- src/components/common/layout/layout.tsx
+++ src/components/common/layout/layout.tsx
@@ -22,7 +22,6 @@ import { remainConnceted } from '../../../features/user/auth/authSlice';
 import { UserRole as role } from '../../../models/role'
 import React from 'react';
 const Gallery = lazy(() => import('../gallery/gallery'));
-import workoutImage from "../../../../public/anastase-maragos-YVz1LxVJqoA-unsplash.jpg";
 
 function Layout() {
 
@@ -50,7 +49,6 @@ function Layout() {
 
   return (
     <div className="layout container-fluid">
-      <img src={workoutImage} alt="workoutImage" className='bacground-image-all-app' />
       <>
         {
           <nav style={{ display: isMenuOpen ? 'block' : 'none' }} >
