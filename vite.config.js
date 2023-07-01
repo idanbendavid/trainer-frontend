6851c9e5a02cddb4eae9d29f6e259299e0441fb9
idanbendavid <idanbendavid10@hotmail.com>
2023-07-01 16:34:47 +0300

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,22 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 
 export default defineConfig(() => {
   return {
     server: {
-        port: 3000
+      port: 3000
     },
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
-  };
-});
+    plugins: [viteCommonjs(), react()],
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [esbuildCommonjs(['react-s3'])],
+      },
+    }
+  }
+})
+
